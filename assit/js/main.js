4af4ada9cd3bb4bb838939fb537e3e6df116f0f4
rota99C <rotaboolean@gmail.com>
2021-10-24 16:10:39 +0200

    esercizio completo

---
+++ assit/js/main.js
@@ -0,0 +1,26 @@
+let team = [
+    {
+        name: "Dybala",
+        role: "attaccante",
+        Image: "https://gianlucadimarzio.com/images/dybala-juventus-image-gpo.jpg?p=intextimg&s=946fe51849a6f8f427087698d412a823"
+
+    },
+    {
+        name: "moise Kean",
+        role: "attaccante",
+        Image: "https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(1000x460:1002x458)/origin-imgresizer.eurosport.com/2019/03/30/2554855-52995810-2560-1440.jpg"
+
+    },
+    {
+        name: "Wojciech Szczęsny",
+        role: "portiere",
+        Image: "https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(984x519:986x517)/origin-imgresizer.eurosport.com/2021/10/20/3239827-66313808-2560-1440.jpg"
+    },
+    {
+        name: "Federico Chiesa",
+        role: "attaccante",
+        Image: "https://tmssl.akamaized.net/images/foto/normal/federico-chiesa-1632222805-71432.jpg?lm=1632222818"
+    }
+]
+
+console.log(team)
